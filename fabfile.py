66fe9078d72434baa24ba22e817dab9528737ff2
ryanglambert <ryan.g.lambert@gmail.com>
2016-02-23 15:55:22 -0800

    check for id_rsa

--- fabfile.py
+++ fabfile.py
@@ -1,10 +1,9 @@
+import os.path
 from fabric.api import run, sudo, env, execute, settings
 from fabric.operations import run, put
 
-user = 'ryanl'
+user = raw_input('what your username will be on server: ')
 
-def host_type():
-    run('uname -s')
 
 def create_user():
     sudo('adduser {user} --gecos \'no, no, no, no, no\' --disabled-password'.format(user=user))
@@ -22,18 +21,24 @@ def copy_ssh_key():
 
 
 def run_provision_script():
-    sudo('apt-get update')
+    # with settings(prompts={'Do you want to continue [Y/n]? ': 'Y'}):
+    sudo('echo "y\n" | apt-get update')
     sudo('apt-get install python-pip python-dev build-essential')
     sudo('pip install --upgrade pip')
 
 # def set_user_perms():
 #     sudo('echo
 
-execute(create_user)
-print("COPYING SSH KEY TO USER " + str(['####'] * 3))
-execute(copy_ssh_key)
-print("RUN PROVISION BASH SCRIPT" + str(['####'] * 3))
+user_home = os.path.expanduser('~')
+ssh_loc = os.path.join(user_home, '.ssh/id_rsa.pub')
 
-with settings(prompts={'Do you want to continue [Y/n]? ': 'Y'}):
+if os.path.isfile('~/.ssh/id_rsa.pub') == True:
+    execute(create_user)
+    print("COPYING SSH KEY TO USER " + str(['####'] * 3))
+    execute(copy_ssh_key)
+    print("RUN PROVISION BASH SCRIPT" + str(['####'] * 3))
+    
     execute(run_provision_script)
 
+else:
+    print "you don't have an id_rsa.pub go make one and put it in your .ssh folder"
